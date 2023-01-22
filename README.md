b893485fb17f4910099c9aaf52e2a29acdd4f40b
r2dd <>
2023-01-22 23:33:47 +0100

    figures

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lorentzian band structures
+
+Collection of scripts to calculate band structures of 1D geometries of Lorentzian scatterers.
+
+## Installation
+Clone repo and run
+```
+pip intall -e .
+```
--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-import matplotlib.pyplot as plt
+"""Contains main code to compute interaction matrices for Lorentzian scatterers
+"""
+
 import jax
 from jax import Array
 import jax.numpy as jnp
 from jax import lax
-import numpy as np
-
-import pdb
 from jax.config import config
 config.update("jax_enable_x64", True)
 
-def chain( N, lc, y ):
+from mpmath import clcos, clsin
+
+import matplotlib.pyplot as plt
+
+import pdb
+
+def chain( N : int, lc : float, y : float ):
     """
     Constructs a Nx3 array containing the coordinates of a chain of length N with lattice constant lc in the xy-plane along the x-axis, moved by an offset y in y-direction
     """
     return jnp.stack( (jnp.arange(N)*lc, jnp.ones(N)*y, jnp.zeros(N)), axis = 1 )
 
-def stacked_chains( args ):
+def stacked_chains( args : list[tuple[int, float, float]] ):
     """
     Constructs a Nx3 array containing the coordinates of a stack of chains from the list given in args by passing them to chain and then concatenating the results.
     """
     return jnp.concatenate( [chain(*arg) for arg in args]  )
 
-def twisted_chains(a, b, l, o, N):
+def twisted_chains(a : int, b : int, l : float, o : float, N : int):
     """
     Returns a stack representing two chains along the x-axis separated by an offset o in y-direction. 
     The stack is comprised of N unit cells and characterized by a Moiré parameter theta = a/b - 1.  
@@ -38,7 +43,7 @@ def dyad( vec1, vec2 ):
     k = 2*jnp.pi
     r_vec = vec1 - vec2
     r = jnp.linalg.norm(r_vec)
-    return lax.cond( r == 0, lambda r : jnp.zeros((3,3), dtype = complex), lambda r : jnp.exp(1j*k*r)/r*( jnp.eye(3)*(1 + (1j*k*r-1)/(k*r)**2 ) + (-1 + (3 - 3*1j*k*r)/(k*r)**2) * jnp.outer(r_vec,r_vec)/r**2 ), r )
+    return lax.cond( r == 0, lambda r : jnp.zeros((3,3), dtype = complex), lambda r : jnp.exp(1j*k*r)/(4*jnp.pi*r)*( jnp.eye(3)*(1 + (1j*k*r-1)/(k*r)**2 ) + (-1 + (3 - 3*1j*k*r)/(k*r)**2) * jnp.outer(r_vec,r_vec)/r**2 ), r )
 
 def reorder( mat, n_m = 3 ):
     """
@@ -50,18 +55,18 @@ def reorder( mat, n_m = 3 ):
         return mat[r,s,l,k]
     return jax.vmap( jax.vmap( jax.jit(inner), (0, None), 0), (None,0), 0 )( jnp.arange(mat.shape[0]*3), jnp.arange(mat.shape[0]*3) )
 
-def int_mat( pos ):
+def int_mat( pos, n_m = 3 ):
     """
-    Maps a Nx3 position array to a 3Nx3N interaction matrix at k = k_0 (scaled by prefactors and without the diagonal part)
+    Maps a Nx3 position array to a 3Nx3N interaction matrix at k = k_0. The diagonal part, which is just 1j times the identity, is omitted.
     """
-    return -1/(4*jnp.pi) * reorder( jax.vmap( jax.vmap( dyad, (0, None), 0 ), (None, 0), 0 )(pos, pos) )
+    return 3 * reorder( jax.vmap( jax.vmap( dyad, (0, None), 0 ), (None, 0), 0 )(pos, pos) ) + 1j * jnp.eye( pos.shape[0] * n_m )
 
 def spectrum( pos ):
     """
-    Returns shift, linewidth and eigenvectors. Shift and linewidth are computed according to the expressions in https://link.springer.com/article/10.1140/epjb/e2020-100473-3
+    Returns shift, linewidth and eigenvectors. Shift and linewidth are computed according to the expressions in https://link.springer.com/article/10.1140/epjb/e2020-100473-3.    
     """
     vals, vecs = jnp.linalg.eig( int_mat(pos) )
-    return -jnp.real( 2*vals  ), -jnp.imag( vals ), vecs
+    return -jnp.real( vals/2  ), jnp.imag( vals ), vecs
 
 def band_structure( vecs, N = 1, n_m = 3 ):
     """
@@ -86,7 +91,7 @@ def dos( w, wm, gm ):
     """
     Computes the DOS according to the expression in https://link.springer.com/article/10.1140/epjb/e2020-100473-3
     """
-    return jnp.sum( gm / ( (jnp.expand_dims(w,1) - wm)**2 + gm**2), axis = 1 )
+    return 1/(wm.size*jnp.pi)*jnp.sum( gm / ( (jnp.expand_dims(w,1) - wm)**2 + gm**2), axis = 1 )
 
 def show( pos ):
     fig, ax = plt.subplots(1,1)
@@ -100,10 +105,11 @@ def show_int_mat( pos ):
 def show_band_structure( pos, n = 1 ):
     shift, linewidth, vecs = spectrum(pos)
     ks = band_structure( vecs, n )
-    fig, axs = plt.subplots(2,1)
-    axs[0].plot( ks, -shift, '.' )
-    axs[1].plot( ks, linewidth, '.' )
-    plt.show()
+    fig, axs = plt.subplots(1,2)
+    scale = 1
+    axs[0].plot( ks, scale*shift, '.' )
+    axs[1].plot( ks, scale*linewidth, '.' )
+    return axs
 
 def show_eigenstate( pos, selection : Array, component = 0, particle = 0, n = 1 ):
     _,_,vecs = spectrum(pos)
@@ -114,6 +120,72 @@ def show_eigenstate( pos, selection : Array, component = 0, particle = 0, n = 1 
 def quickshow( vec ):
     plt.plot( jnp.arange(vec.size), vec )
     plt.show()
+
+def analytic_chain( l : float, kzs ):
+    """
+    Analytic expression for dispersion relation and collective decay rates of 1D chain with lattice constant l, evaluated at k-vectors given by the array kzs
+    Input:
+    l : Lattice constant
+    kzs : N-dim Array containing k vectors
+    Output:
+    res : Nx4 array where res[:,:2] contains delta and res[:,2:] contains gamma
+    """
+    def inner( kz : float ) -> tuple[float, float, float, float] :
+        arg_plus, arg_minus = klc + kz, klc - kz
+        F1 = 1j*(clsin( 1, arg_plus ) + clsin( 1, arg_minus )) + clcos( 1, arg_plus ) + clcos( 1, arg_minus )
+        F2 = 1j*(clsin( 2, arg_plus ) + clsin( 2, arg_minus )) + clcos( 2, arg_plus ) + clcos( 2, arg_minus )
+        F3 = 1j*(clsin( 3, arg_plus ) + clsin( 3, arg_minus )) + clcos( 3, arg_plus ) + clcos( 3, arg_minus )
+        G_11 = complex( F1/klc + 1j*F2/klc**2 - F3/klc**3 )
+        G_33 = 2*complex( -1j*F2/klc**2 + F3/klc**3 )
+        return [-3/4*jnp.real(G_11), -3/4*jnp.real(G_33), 3/2*jnp.imag(G_11) + 1, 3/2*jnp.imag(G_33) + 1]
+    klc = 2*jnp.pi*l
+    return jnp.array( [ inner(float(x)) for x in kzs ]  )
+
+def analytic_stack( l : float, o : float, kzs ):
+    chain = 2*analytic_chain( l, kzs )
+    bands = analytic_flat_bands( 1, 1, l, o ) * jnp.ones_like( kzs )
+    return jnp.stack( (chain[:,0] - bands[0,0], chain[:,1] - bands[0,0], chain[:,0] - bands[1,0], bands[0,:], bands[1,:], bands[1,:],
+                             chain[:,2], chain[:,3], chain[:,2], bands[2,:], bands[3,:], bands[2,:]) ).T
+                             
+
+def analytic_flat_bands( a : int, b : int, l : float, o : float ):
+    pos = twisted_chains( a, b, l, o, 1)
+    kr  = 2*jnp.pi*jnp.min(jnp.linalg.norm(pos[ pos[:,1] != 0 ] - jnp.expand_dims(pos[ pos[:,1] == 0 ],1), axis = 2), axis = 1)
+    E1 = 3/(4*kr) * ( jnp.cos(kr) - jnp.cos(kr)/kr**2 - jnp.sin(kr)/kr )
+    E2 = 3/(2*kr) * ( jnp.cos(kr)/kr**2 + jnp.sin(kr)/kr )
+    return jnp.array( [E1, E2, jnp.zeros_like(E1), jnp.zeros_like(E2)] )
+
+k = jnp.linspace(0, 1, 100)
+
+## single chain 
+# lc = 0.2
+# axs = show_band_structure( chain(40,lc,0) )
+# arr = analytic_chain( lc, k*jnp.pi )
+# axs[0].plot( k, arr[:,:2] )
+# axs[1].plot( k, arr[:,2:] )
+# plt.show()
+# plt.close()
+
+## stack
+# lc, o = 0.3, 0.1
+# arr = analytic_stack( lc, o, jnp.linspace(0, 1, 100)*jnp.pi)
+# axs = show_band_structure( twisted_chains(1,1,lc,o,30), 2 )
+# axs[0].plot( k, arr[:,:6] )
+# axs[1].plot( k, arr[:,6:] )
+# plt.show()
+# plt.close()
+
+## dos
+w = jnp.linspace(-8,8,100)
+plt.plot( dos(w, *spectrum(twisted_chains(1,1,lc,o,30))[:2]  ) )
+plt.show()
+
+## twisted chains
+
+
+# w = jnp.linspace(-4,4,100)
+# plt.plot( dos( w, *spectrum(chain(20, 0.2, 0))[:2] ) )
+# plt.show()
     
 # show_band_structure(chain(20, 0.2, 0))
 # show_band_structure( twisted_chains(1,1,0.3,0.1,30), 2 )
