663391540e2e9e7e351b36a7bd4437487cfdff43
r2dd <>
2023-01-26 20:27:29 +0100

    plots

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ from matplotlib.colorbar import Colorbar
 
 import pdb
 
+# globals, treat read only, these variables have no influence on the simulations as long as spatial quantities are measured in units of LAMBDA_0
+LAMBDA_0 =  1
+K_0      = 2*jnp.pi / LAMBDA_0
+SCALE    = -6*jnp.pi/K_0**3 * K_0**2
+
+
 def chain( N : int, lc : float, y : float ):
     """
     Constructs a Nx3 array containing the coordinates of a chain of length N with lattice constant lc in the xy-plane along the x-axis, moved by an offset y in y-direction
@@ -43,10 +49,9 @@ def dyad( vec1, vec2 ):
     """
     Computes the 3x3 dyadic Green's tensor at k = k_0 connecting vec1 and vec2
     """    
-    k = 2*jnp.pi
     r_vec = vec1 - vec2
     r = jnp.linalg.norm(r_vec)
-    return lax.cond( r == 0, lambda r : jnp.zeros((3,3), dtype = complex), lambda r : jnp.exp(1j*k*r)/(4*jnp.pi*r)*( jnp.eye(3)*(1 + (1j*k*r-1)/(k*r)**2 ) + (-1 + (3 - 3*1j*k*r)/(k*r)**2) * jnp.outer(r_vec,r_vec)/r**2 ), r )
+    return lax.cond( r == 0, lambda r : jnp.zeros((3,3), dtype = complex), lambda r : jnp.exp(1j*K_0*r)/(4*jnp.pi*r)*( jnp.eye(3)*(1 + (1j*K_0*r-1)/(K_0*r)**2 ) + (-1 + (3 - 3*1j*K_0*r)/(K_0*r)**2) * jnp.outer(r_vec,r_vec)/r**2 ), r )
 
 def reorder( mat, n_m = 3 ):
     """
@@ -62,7 +67,7 @@ def int_mat( pos, n_m = 3 ):
     """
     Maps a Nx3 position array to a 3Nx3N interaction matrix at k = k_0. The diagonal part, which is just 1j times the identity, is omitted.
     """
-    return 3 * reorder( jax.vmap( jax.vmap( dyad, (0, None), 0 ), (None, 0), 0 )(pos, pos) ) + 1j * jnp.eye( pos.shape[0] * n_m )
+    return -SCALE * reorder( jax.vmap( jax.vmap( dyad, (0, None), 0 ), (None, 0), 0 )(pos, pos) ) + 1j * jnp.eye( pos.shape[0] * n_m )
 
 def spectrum( pos ):
     """
@@ -132,15 +137,20 @@ def analytic_chain( l : float, kzs ):
     res : Nx4 array where res[:,:2] contains delta and res[:,2:] contains gamma
     """
     def inner( kz : float ) -> tuple[float, float, float, float] :
-        arg_plus, arg_minus = klc + kz, klc - kz
-        F1 = 1j*(clsin( 1, arg_plus ) + clsin( 1, arg_minus )) + clcos( 1, arg_plus ) + clcos( 1, arg_minus )
-        F2 = 1j*(clsin( 2, arg_plus ) + clsin( 2, arg_minus )) + clcos( 2, arg_plus ) + clcos( 2, arg_minus )
-        F3 = 1j*(clsin( 3, arg_plus ) + clsin( 3, arg_minus )) + clcos( 3, arg_plus ) + clcos( 3, arg_minus )
-        G_11 = complex( F1/klc + 1j*F2/klc**2 - F3/klc**3 )
-        G_33 = 2*complex( -1j*F2/klc**2 + F3/klc**3 )
-        return [-3/4*jnp.real(G_11), -3/4*jnp.real(G_33), 3/2*jnp.imag(G_11) + 1, 3/2*jnp.imag(G_33) + 1]
-    klc = 2*jnp.pi*l
-    return jnp.array( [ inner(float(x)) for x in kzs ]  )
+        arg_plus, arg_minus = k_null_dimless + kz, k_null_dimless - kz
+        F1 = clcos( 1, arg_plus ) + clcos( 1, arg_minus )
+        F2 = (clsin( 2, arg_plus ) + clsin( 2, arg_minus )) / k_null_dimless
+        F3 = (clcos( 3, arg_plus ) + clcos( 3, arg_minus )) / k_null_dimless**2
+        delta_parallel     =  float( F2 + F3 ) 
+        delta_perp = 1/2*float( F1 - F2 - F3 ) 
+
+        gamma_parallel = (k_null_dimless**2 - kz**2)/k_null_dimless**2 if k_null_dimless > kz else 0
+        gamma_perp     = 1/2*(k_null_dimless**2 + kz**2)/k_null_dimless**2 if k_null_dimless > kz else 0
+
+        return [ scale_d*delta_perp, scale_d*delta_parallel, scale_g*gamma_perp, scale_g*gamma_parallel ]
+    
+    k_null_dimless, scale_d, scale_g = K_0*l, SCALE/(4*jnp.pi*l), -SCALE/(4*l)
+    return jnp.array( [ inner(float(x)) for x in kzs * l ]  )
 
 def analytic_stack( l : float, o : float, kzs ):
     chain = 2*analytic_chain( l, kzs )
@@ -150,9 +160,10 @@ def analytic_stack( l : float, o : float, kzs ):
 
 def analytic_flat_bands( a : int, b : int, l : float, o : float ):
     pos = twisted_chains( a, b, l, o, 1)
-    kr  = 2*jnp.pi*jnp.min(jnp.linalg.norm(pos[ pos[:,1] != 0 ] - jnp.expand_dims(pos[ pos[:,1] == 0 ],1), axis = 2), axis = 1)
-    E1 = 3/(4*kr) * ( jnp.cos(kr) - jnp.cos(kr)/kr**2 - jnp.sin(kr)/kr )
-    E2 = 3/(2*kr) * ( jnp.cos(kr)/kr**2 + jnp.sin(kr)/kr )
+    r  = jnp.min(jnp.linalg.norm(pos[ pos[:,1] != 0 ] - jnp.expand_dims(pos[ pos[:,1] == 0 ],1), axis = 2), axis = 1)
+    kr = K_0 * r
+    E1 = -SCALE/(8*jnp.pi*r) * ( jnp.cos(kr) - jnp.cos(kr)/kr**2 - jnp.sin(kr)/kr )
+    E2 = -SCALE/(4*jnp.pi*r) * ( jnp.cos(kr)/kr**2 + jnp.sin(kr)/kr )
     return jnp.stack( [E1, E2, jnp.zeros_like(E1), jnp.zeros_like(E2)] )
 
 k = jnp.linspace(0, 1, 200)
@@ -168,10 +179,11 @@ LL_SIZE = 2.1
 LL_LABEL = r'$k = k_0$'
 
 ## single chain
-# lc = 0.2
+# lc = 0.2 * LAMBDA_0
+# light_line = K_0 / (jnp.pi/lc) 
 # shift, linewidth, vecs = spectrum( chain(40,lc,0) )
 # ks = band_structure( vecs, 1 )
-# arr = analytic_chain( lc, k*jnp.pi )
+# arr = analytic_chain( lc, k*jnp.pi/lc )
 
 # fig, (ax1, ax2) = plt.subplots(ncols=2)
 # ax1.set_box_aspect(1)
@@ -179,7 +191,7 @@ LL_LABEL = r'$k = k_0$'
 # ax1.plot( ks, shift, '.', lw = PS )
 # ax1.set_xlabel( X_LABEL )
 # ax1.set_ylabel( D_LABEL )
-# ax1.axvline( 2*lc, c = LL_COLOR, ls = LL_STYLE, alpha = LL_ALPHA, lw = LL_SIZE )
+# ax1.axvline( light_line, c = LL_COLOR, ls = LL_STYLE, alpha = LL_ALPHA, lw = LL_SIZE )
 # ax1.legend( ax1.lines, (r'$D_{\perp}(k)$', r'$D_{\parallel}$(k)', 'finite', LL_LABEL ) )
 
 # ax2.set_box_aspect(1)
@@ -187,62 +199,74 @@ LL_LABEL = r'$k = k_0$'
 # ax2.plot( ks, linewidth, '.', lw = PS )
 # ax2.set_xlabel( X_LABEL )
 # ax2.set_ylabel( G_LABEL )
-# ax2.axvline( 2*lc, c = LL_COLOR, ls = LL_STYLE, alpha = LL_ALPHA, lw = LL_SIZE )
+# ax2.axvline( light_line, c = LL_COLOR, ls = LL_STYLE, alpha = LL_ALPHA, lw = LL_SIZE )
 # ax2.legend( ax2.lines, (r'$G_{\perp}(k)$', r'$G_{\parallel}(k)$', 'finite', LL_LABEL) )
 # plt.savefig('single_chain.pdf')
 # plt.show()
 # plt.close()
 
 ## stack with no Moiré
-lc, o = 0.3, 0.1
-arr = analytic_stack( lc, o, k * jnp.pi)
-pos = twisted_chains(1,1,lc,o,30)
-shift, linewidth, vecs = spectrum( pos )
-w = jnp.linspace(jnp.min(shift)-1,jnp.max(shift)+1,400)
-ks = band_structure( vecs, 2 )
-d = dos(w, shift, linewidth  ) 
+# lc, o = 0.3*LAMBDA_0, 0.1*LAMBDA_0
+# light_line = K_0 / (jnp.pi/lc) 
+# arr = analytic_stack( lc, o, k * jnp.pi/lc)
+# pos = twisted_chains(1,1,lc,o,30)
+# shift, linewidth, vecs = spectrum( pos )
+# w = jnp.linspace(jnp.min(shift)-1,jnp.max(shift)+1,400)
+# ks = band_structure( vecs, 2 )
+# d = dos(w, shift, linewidth  ) 
 
-fig = plt.figure()
-canvas = gridspec.GridSpec(2,
-                          1,
-                          wspace=0,
-                          hspace=0.05,
-                          height_ratios=[0.05,1])
-inlet = gridspec.GridSpecFromSubplotSpec(1,
-                                         2,
-                                         subplot_spec=canvas[1],
-                                         wspace=0,
-                                         hspace=0)
+# fig = plt.figure()
+# canvas = gridspec.GridSpec(2,
+#                           1,
+#                           wspace=0,
+#                           hspace=0.05,
+#                           height_ratios=[0.05,1])
+# inlet = gridspec.GridSpecFromSubplotSpec(1,
+#                                          2,
+#                                          subplot_spec=canvas[1],
+#                                          wspace=0,
+#                                          hspace=0)
 
-ax1 = plt.subplot( inlet[0] )
-sc = ax1.scatter( ks, shift, c = linewidth )
-ax1.plot( k, arr[:,:6] )
-ax1.set_xlabel( X_LABEL )
-ax1.set_ylabel( 'D' )
-ax1.axvline( 2*lc, c = LL_COLOR, ls = LL_STYLE, alpha = LL_ALPHA, lw = LL_SIZE )
+# ax1 = plt.subplot( inlet[0] )
+# sc = ax1.scatter( ks, shift, c = linewidth )
+# ax1.plot( k, arr[:,:6] )
+# ax1.set_xlabel( X_LABEL )
+# ax1.set_ylabel( 'D' )
+# ax1.axvline( light_line, c = LL_COLOR, ls = LL_STYLE, alpha = LL_ALPHA, lw = LL_SIZE )
 
-ax2 = plt.subplot( inlet[1] )
-ax2.sharey(ax1)
-ax2.plot( d, w, '--' )
-ax2.set_xlabel( r'DOS' )
-ax2.legend( ax1.lines[:-1] + ax2.lines, (r'$D_{1,+}(k)$', r'$D_{3,+}(k)$', r'$D_{2,+}(k)$', r'$D_{1,-}(k)$', r'$D_{3,-}(k)$', r'$D_{2,-}(k)$', r'$DOS(D)$' ) )
-plt.setp(ax2.get_yticklabels(), visible=False)
+# ax2 = plt.subplot( inlet[1] )
+# ax2.sharey(ax1)
+# ax2.plot( d, w, '--' )
+# ax2.set_xlabel( r'DOS' )
+# ax2.legend( ax1.lines[:-1] + ax2.lines, (r'$D_{1,+}(k)$', r'$D_{3,+}(k)$', r'$D_{2,+}(k)$', r'$D_{1,-}(k)$', r'$D_{3,-}(k)$', r'$D_{2,-}(k)$', r'$DOS(D)$' ) )
+# plt.setp(ax2.get_yticklabels(), visible=False)
 
-cbax = plt.subplot(canvas[0])
-cb = Colorbar(ax = cbax, mappable = sc, orientation = 'horizontal', ticklocation = 'top', label = r'$G$' )
+# cbax = plt.subplot(canvas[0])
+# cb = Colorbar(ax = cbax, mappable = sc, orientation = 'horizontal', ticklocation = 'top', label = r'$G$' )
 # plt.show()
 # plt.close()
-plt.savefig('dos.pdf')
+# plt.savefig('dos.pdf')
 
 ## twisted chains
-# a, b, o, lc = 3, 4, 0.05, 0.3
-# arr = analytic_flat_bands( a, b, lc, o)
-# shift, linewidth, vecs = spectrum( twisted_chains(a,b,lc,o,30) )
-# ks = band_structure( vecs, a+b )
+fig, ax = plt.subplots()
+a, b, o, lc = 3, 4, 0.05*LAMBDA_0, 0.3*LAMBDA_0
+vals = [ (3,4), (5,7), (8,9) ]
+flat_bands = [ analytic_flat_bands( a, b, lc, o) for a,b in vals ]
+lim1, lim2 = max( [ jnp.max(arr) for arr in flat_bands ] ) + 10, min( jnp.min(arr) for arr in flat_bands ) - 10
+for i,arr in enumerate(flat_bands):
+    a,b = vals[i]
+    shift, linewidth, vecs = spectrum( twisted_chains(a,b,lc,o,30) )
+    w = jnp.linspace(lim1,lim2,4000)
+    d = dos(w, shift, linewidth) 
 
-# fig, (ax1, ax2) = plt.subplots(ncols=2)
-# ax1.plot( ks, shift, '.' )
-# for el in arr[:int(arr.shape[0]/2),:].flatten():
-#     ax1.axhline( el )
+    ax.plot( w, d, label = rf'$\theta = {a}/{b}$' )
+    for el in arr[:int(arr.shape[0]/2),:].flatten():
+        ax.axvline( el, ls = '--', c = ax.lines[-1].get_color() )
+
+ax.set_yscale('log')
+ax.set_xlabel(r'$D$')
+ax.set_ylabel(r'$\log(DOS)$')
+plt.legend()
 # plt.show()
 # plt.close()
+plt.savefig('dos2.pdf')
